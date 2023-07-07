7d3a4d739de9d5869007f58e641e90ed1202ded1
Semigut <semigut@gmx.de>
2023-07-07 23:41:19 +0200

    Spawner

---
+++ gmtk/Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+    public float velocity = 1;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector2.up * velocity;
+
+        }
+    }
+}
---
+++ gmtk/Assets/SpawnerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+
+    public float maxTime = 1;
+    private float timer = 0.5f;
+    public GameObject spawn;
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( timer > maxTime )
+        {
+
+            GameObject newspaner = Instantiate(spawn);
+            newspaner.transform.position = transform.position + new Vector3 ( 0, Random.Range(-height, height), 0 );
+            Destroy(newspaner, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
