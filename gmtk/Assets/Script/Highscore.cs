6d410e9b71fa84624092a3e98e59359471a4298c
Semigut <semigut@gmx.de>
2023-07-08 09:58:42 +0200

    Highscore

---
+++ gmtk/Assets/Script/Highscore.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public static Highscore instance;
+
+    public Text highscore;
+    private int score = 0;
+
+   private void Awake() { 
+
+        instance = this;
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore.text = "Highscore: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AppPoint()
+    {
+        score += 1;
+        highscore.text = "Highscore: " + score.ToString();
+    }
+}
+    
